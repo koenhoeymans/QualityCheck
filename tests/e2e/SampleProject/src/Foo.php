65e28fd0af33c613d7398af05ec513da55accceb
koenhoeymans <koenhoeymans@gmail.com>
2017-12-16 17:42:45 +0100

    add return type and remove phpdoc

--- tests/e2e/SampleProject/src/Foo.php
+++ tests/e2e/SampleProject/src/Foo.php
@@ -4,10 +4,8 @@ namespace Foo;
 
 class Foo
 {
-    /**
-     * this function returns 3
-     */
-    public function returnsInt3() {
+    public function returnsInt3() : int
+    {
         $a = 1;
         $b = 2;
         return $a+$b;
